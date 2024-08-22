89b00c34ecbc143de678a01f9a1dbd3c16d4956f
jobscale <jobscale@users.noreply.github.com>
2024-08-23 00:41:50 +0900

    container memory updated

--- run-docker.sh
+++ run-docker.sh
@@ -11,8 +11,8 @@ debian-kde() {
   -e USER=jobscale \
   -p 2999:2999 \
   -p 2997:8000 \
-  --memory 3g \
-  --shm-size 2g \
+  --memory 4g \
+  --shm-size 4g \
   -v /home/webtop/debian-kde:/config \
   -d ghcr.io/jobscale/docker-webtop
 
